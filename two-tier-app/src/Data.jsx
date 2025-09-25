8e48c2e344a68e1e10e849febf25f479d3473223
bolingxli <bolingxli@gmail.com>
2025-09-25 12:48:29 +0100

    Add React app folder as part of main repo

---
+++ two-tier-app/src/Data.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function Data() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://54.76.111.111:8000/data")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+      })
+      .catch((err) => console.error("API call error:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        Hi there I am here to test my two tier application. Hope this works
+        lol😂
+      </h1>
+      {data ? <div>{data.message}</div> : "Loading..."}
+    </div>
+  );
+}
+
+export default Data;
